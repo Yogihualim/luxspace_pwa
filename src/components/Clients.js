86b2abc0bf217178481ea2b1ea13f492a2be29f9
Yogi Hua <yogihualim@gmail.com>
2024-10-23 16:33:02 +0700

    Assets Change

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -4,28 +4,28 @@ function Clients() {
       <div className="flex justify-center flex-wrap">
         <div className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0">
           <img
-            src="images/content/logo-adobe.svg"
+            src="./images/content/logo-adobe.svg"
             alt=""
             className="mx-auto"
           />
         </div>
         <div className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0">
           <img
-            src="images/content/logo-ikea.svg"
+            src="./images/content/logo-ikea.svg"
             alt=""
             className="mx-auto"
           />
         </div>
         <div className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0">
           <img
-            src="images/content/logo-hermanmiller.svg"
+            src="./images/content/logo-hermanmiller.svg"
             alt=""
             className="mx-auto"
           />
         </div>
         <div className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0">
           <img
-            src="images/content/logo-miele.svg"
+            src="./images/content/logo-miele.svg"
             alt=""
             className="mx-auto"
           />
